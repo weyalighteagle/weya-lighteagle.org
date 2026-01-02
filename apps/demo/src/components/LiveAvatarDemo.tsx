5981c3db0de116b13b3cd94634043d74ff51de49
yigit kandemir <yigitkand3mir@yigits-MacBook-Air.local>
2026-01-02 11:39:20 +0100

    fix: add missing avatar-styles.css for demo app

--- apps/demo/src/components/LiveAvatarDemo.tsx
+++ apps/demo/src/components/LiveAvatarDemo.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import { LiveAvatarSession } from "./LiveAvatarSession";
+import "./avatar-styles.css";
 import { useRouter } from "next/navigation";
 
 type Props = {
