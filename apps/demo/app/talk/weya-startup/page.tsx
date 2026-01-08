13ab828e42651dd5ad46f6ae6a713fc58b317f15
yigit kandemir <yigitkand3mir@yigits-MacBook-Air.local>
2026-01-08 21:31:57 +0100

    fix: add missing avatar-styles.css for demo app

--- apps/demo/app/talk/weya-startup/page.tsx
+++ apps/demo/app/talk/weya-startup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LiveAvatarDemo } from "../../../src/components/LiveAvatarDemo";
+import { LiveAvatarDemo } from "../../../src/components/LiveAvatarStartupInterview";
 
 export default function WeyaStartupPage() {
   return <LiveAvatarDemo persona="weya_startup" />;
